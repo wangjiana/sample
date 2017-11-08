75895e085424802e3db37d0d20a58fffada609e9
vagrant <vagrant@homestead>
2017-11-08 12:17:24 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
@@ -14,5 +15,7 @@
         @include('layouts._footer')
     </div>
 </div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
